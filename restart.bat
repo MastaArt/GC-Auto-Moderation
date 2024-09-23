3dab0af600d7cc9f449e6028606f3bccc49f4c7c
MAXSCRIPT <lukuanenko@gmail.com>
2024-09-23 15:44:37 +0300

    1.3.8

---
+++ restart.bat
@@ -0,0 +1,28 @@
+:: Restarter
+:: 1.0.0
+:: Vasyl Lukianenko 
+:: 3DGROUND
+:: https://3dground.net
+
+echo off
+setlocal enabledelayedexpansion
+cls
+
+set input=%1
+set run=
+set adv=
+
+
+for /f "tokens=1,2 delims=;" %%i in (%input%) do (
+	set run=%%i
+	set adv=%%j
+)
+
+taskkill /F /IM 3dsmax.exe
+
+:: 10sec pause
+ping -n 11 127.0.0.1 >nul
+
+start "" "%run%"
+
+exit 0
